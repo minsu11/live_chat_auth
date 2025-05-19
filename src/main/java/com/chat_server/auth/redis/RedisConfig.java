c80ca481e50728b0bbc6b1b80b14046cc9db2a49
minsu11 <zmfjtnl1@naver.com>
2025-05-19 17:58:05 +0900

    [Feat] Redis config 생성

---
+++ src/main/java/com/chat_server/auth/redis/RedisConfig.java
@@ -0,0 +1,16 @@
+package com.chat_server.auth.redis;
+
+/**
+ * packageName    : com.chat_server.auth.redis
+ * fileName       : RedisConfig
+ * author         : parkminsu
+ * date           : 25. 5. 19.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 25. 5. 19.        parkminsu       최초 생성
+ */
+
+public class RedisConfig {
+}
