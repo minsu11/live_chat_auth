7954ea3bc3f72a7aa41c747f6f82eb7b00e70a43
minsu11 <zmfjtnl1@naver.com>
2025-05-20 11:17:38 +0900

    [Feat] auth server redis config bean등록

--- src/main/java/com/chat_server/auth/redis/RedisConfig.java
+++ src/main/java/com/chat_server/auth/redis/RedisConfig.java
@@ -1,5 +1,12 @@
 package com.chat_server.auth.redis;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
 /**
  * packageName    : com.chat_server.auth.redis
  * fileName       : RedisConfig
@@ -11,6 +18,21 @@ package com.chat_server.auth.redis;
  * -----------------------------------------------------------
  * 25. 5. 19.        parkminsu       최초 생성
  */
-
+@Configuration
 public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+        ObjectMapper mapper = new ObjectMapper();
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+//        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
 }
