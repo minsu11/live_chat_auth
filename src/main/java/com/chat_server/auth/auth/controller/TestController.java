63b6e4d253b767101ddca367c9a6d89109ba4555
minsu <zmfjtnl1@naver.com>
2025-09-08 17:36:59 +0900

    [Test] 배포 서버 env 파일 적용 및 nginx 테스트 위한 테스트 컨트롤러 api 경로 변경

--- src/main/java/com/chat_server/auth/auth/controller/TestController.java
+++ src/main/java/com/chat_server/auth/auth/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class TestController {
 
-    @GetMapping("/test")
+    @GetMapping("/auth/test")
     public String test() {
         return "test";
     }
