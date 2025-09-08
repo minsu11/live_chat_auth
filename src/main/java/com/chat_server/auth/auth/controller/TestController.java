5ec1da4a5242a93637b2ba540caf15dc6f1394b6
minsu <zmfjtnl1@naver.com>
2025-09-08 19:05:05 +0900

    [Test] test

--- src/main/java/com/chat_server/auth/auth/controller/TestController.java
+++ src/main/java/com/chat_server/auth/auth/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class TestController {
 
-    @GetMapping("/health/test")
+    @GetMapping("/test")
     public String test() {
         return "test";
     }
