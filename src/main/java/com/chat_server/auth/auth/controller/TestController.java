251a2a662e672e5fe1e59c2d21d1c27a94ef05f5
minsu <zmfjtnl1@naver.com>
2025-09-14 14:41:09 +0900

    test

--- src/main/java/com/chat_server/auth/auth/controller/TestController.java
+++ src/main/java/com/chat_server/auth/auth/controller/TestController.java
@@ -1,14 +1,16 @@
 package com.chat_server.auth.auth.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@Slf4j
 public class TestController {
 
     @GetMapping("/minsu/chat")
     public String test() {
+        log.warn("test 들어옴");
         return "test";
     }
 }
